85099f9f58d33635cb99e8769d4b1bac014f8bdd
cuiwz <cuiwzupc@163.com>
2020-04-18 15:44:29 +0800

    money refund

--- shop-coupon-service/src/main/java/com/cuiwz/mq/CancelMQListener.java
+++ shop-coupon-service/src/main/java/com/cuiwz/mq/CancelMQListener.java
@@ -12,6 +12,7 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
 
@@ -29,6 +30,7 @@ public class CancelMQListener implements RocketMQListener<MessageExt> {
     private TradeCouponMapper couponMapper;
 
     @Override
+    @Transactional
     public void onMessage(MessageExt message) {
 
         try {
